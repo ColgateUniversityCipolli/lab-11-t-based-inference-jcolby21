555ab910ccf84e95766e1cdfa74f661e23311a65
Jackson Colby <jcolby@colgate.edu>
2025-04-08 14:09:00 -0400

    Question 2 Complete

--- Lab11.R
+++ Lab11.R
@@ -4,3 +4,12 @@ library(pwr)
 result <- pwr.t.test(d = 0.65, sig.level = 0.05, power = 0.80, type = "one.sample")
 print(result)
 #It would take 21 observations to detect moderate-to-large effect d=0.65
+
+
+#Question 2
+library(tidyverse)
+library(readxl)
+finch.dat <- read_excel("Lab11data.xlsx")
+
+finch.tib = tibble(finch.dat) |>
+  mutate(diff_vals=closer_vals - further_vals)
